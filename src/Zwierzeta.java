ac2e004f5f04a13746f9201e339f1e5cff9ca454
Olkaty <olkaty2005@gmail.com>
2025-11-30 20:13:27 +0100

    Update Zwierzeta.java

--- src/Zwierzeta.java
+++ src/Zwierzeta.java
@@ -1,7 +1,29 @@
-public class Zwierzeta {
+public abstract class Zwierzeta
+{
+
     int wiek;
-    int waga;
-    enum wielkość{
-        mały, średni, duży, ogromy
+    int waga; // Moim zdaniem trzeba zrobić wielkość względem wagi (wtedy enum chyba nie potrzebny bo w innym miejscu)
+    String gatunek;
+    int index;
+
+    public enum wielkosc
+    {
+        maly, sredni, duzy, ogromy
     }
+
+    protected wielkosc wielkosc;
+
+
+
+
+    int portwel;
+    // Portwel osoby (można nie trzeba)?
+
+
+
+    //Comparable lub Comparator dla wagi
+
+    // Dodać nowe klasy zwierząt
+
+
 }
